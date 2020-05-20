5c23e19c06e7afdaedb431e89a21daa43c46ccd2
Xenthio <thegooster88@gmail.com>
2020-05-20 11:24:01 +1000

    works

--- interpreter.cpp
+++ interpreter.cpp
@@ -1,23 +1,95 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
+
+std::string prev = "unset";
+std::string prev2 = "unset";
+std::string operation = "unset";
+int output = 0;
+int tick = 0;
+int column = 0;
+int line = 0;
+
+bool is_number(const std::string& s)
+{
+    std::string::const_iterator it = s.begin();
+    while (it != s.end() && std::isdigit(*it)) ++it;
+    return !s.empty() && it == s.end();
+}
+
+void error(int type, int line, int column) {
+	if (type == 1) {
+		std::cout << "Error: Line " + std::to_string(line) + "char " + std::to_string(column) + "," << std::endl;
+		std::cout << "Expected Operation" << std::endl;
+	}
+}
+
+void process(std::string token) {
+	tick += 1;
+	if (tick == 1) {
+		int i;
+		std::istringstream(token) >> i;
+		output += i;
+	}
+	std::cout << token;
+	if (prev == "+") {
+		int i;
+		std::istringstream(token) >> i;
+		output += i;
+	} else if (is_number(prev2)){
+		error(1, line, tick);
+	}
+	prev = token;
+}
 
 int main ()
 {
+	column = 0;
+	line = 0;
   std::ifstream file("input.spp");
   std::string str;
   while (std::getline(file, str)) {
-    std::cout << str << "\n";
-  }
-	std::string s = str;
-	std::string delimiter = " ";
+		line += 1;
+		std::string s = str;
+		std::string delimiter = " ";
+		output = 0;
+		prev = "unset";
+		prev2 = "unset";
+		operation = "unset";
+		tick = 0;
+		size_t pos = 0;
+		std::string token;
+		while ((pos = s.find(delimiter)) != std::string::npos) {
+				token = s.substr(0, pos);
+				process(token);
 
-	size_t pos = 0;
-	std::string token;
-	while ((pos = s.find(delimiter)) != std::string::npos) {
-	    token = s.substr(0, pos);
-	    std::cout << token << std::endl;
-	    s.erase(0, pos + delimiter.length());
-	}
-	std::cout << s << std::endl;
+
+				//std::cout << first + operation + second << std::endl;
+				//std::cout << token << std::endl;
+				s.erase(0, pos + delimiter.length());
+		}
+		process(s);
+		std::cout << std::endl << output << std::endl;
+		// if (second == "unset") {
+		// 	second = s;
+		// } else {
+		// 	last = s;
+		// }
+		// if (operation == "+") {
+		// 	int a;
+		// 	int b;
+		// 	std::istringstream(first) >> a;
+		// 	std::istringstream(second) >> a;
+		// 	std::cout << std::to_string(a) + "+" + std::to_string(b) + "=" <<std::endl;
+		// 	std::cout << std::to_string(a + b) << std::endl;
+		//
+		// } else {
+		// 	std::cout << "Error: Line " + std::to_string(line) + "char 2," << std::endl;
+		// 	std::cout << "Expected Operation" << std::endl;
+		// }
+
+		//std::cout << first + operation + second << std::endl;
+  }
+
 }
